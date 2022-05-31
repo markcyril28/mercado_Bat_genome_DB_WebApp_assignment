1527755ac04bf4e25a88e53131b8d96f571c47f9
Mark Cyril <mercadomarkcyril28@gmail.com>
2022-05-31 15:53:11 +0800

    readded my-first-react-app folder.

---
+++ my-first-react-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+	<h1> Hello World. My name is Mark. This is my first React App and I am learning how to create it. </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
